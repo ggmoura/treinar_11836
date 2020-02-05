d89b83260a32da799d4517b0835afb1269eeb2fc
Treinar <Treinar@MICRO43>
2020-02-05 20:15:30 -0200

    Atualização do Codigo!!!

--- maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/calc/Calc.java
+++ maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/calc/Calc.java
@@ -1,5 +1,12 @@
 package br.com.maxdoliveira.calc;
 
 public class Calc {
-
+	
+	
+	public Integer soma(Integer a, Integer b) {
+		Integer res;
+		res = a + b;
+		return res;
+	}
+		
 }
--- maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/view/Tela.java
+++ maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/view/Tela.java
@@ -2,19 +2,42 @@ package br.com.maxdoliveira.view;
 
 import java.util.Scanner;
 
+import br.com.maxdoliveira.calc.Calc;
+
 public class Tela {
-	
+
 	Scanner teclado = new Scanner(System.in);
-	public Integer numUm, numDois, operador; 
-	
+	Integer numUm; 
+	Integer numDois;
+	Integer operador;
+
+	public String menuOp() {
+		return 	  "1 - Soma\n" 
+				+ "2 - Subtração\n" 
+				+ "3 - Divisão\n"
+				+ "4 - Multiplicação";
+	}
+
 	public void iniciar() {
-		
+
 		System.out.print(" Favor entrar com o Primeiro Numero!");
 		numUm = teclado.nextInt();
 		System.out.print(" Favor entrar com o Segundo Numero!");
 		numDois = teclado.nextInt();
 		System.out.println("Qual operação deseja realizar?");
+		System.out.println(menuOp());
 		operador = teclado.nextInt();
-		
+
+		Calc op = new Calc();
+
+		switch (operador) {
+		case 1:
+			Integer resultado = op.soma(numUm, numDois);
+			System.out.println("O Resutado da operação é " + resultado);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
