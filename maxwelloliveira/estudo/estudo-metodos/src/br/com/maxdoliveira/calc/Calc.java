9274d9239316e3f24217ba3f7092c1606b14f916
maxwelloliveira <maxwelloliveira@STL-NTMAXWELL.irani.local>
2020-02-06 17:23:19 -0300

    Implenmentação das outras operações

--- maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/calc/Calc.java
+++ maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/calc/Calc.java
@@ -1,5 +1,30 @@
 package br.com.maxdoliveira.calc;
 
 public class Calc {
+	
+		
+	public Integer soma(Integer a, Integer b) {
+		Integer res = a + b;
+		return res;
+	}
 
+	public Integer sub(Integer a, Integer b) {
+		Integer res = a - b;
+		return res;
+	}
+
+	public Integer mult(Integer a, Integer b) {
+		Integer res = a * b;
+		return res;
+	}
+
+	public Integer div(Integer a, Integer b) {
+		Integer res = a / b;
+		return res;
+	}
+
+	public Integer mod(Integer a, Integer b) {
+		Integer res = a % b;
+		return res;
+	}
 }
--- maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/view/Tela.java
+++ maxwelloliveira/estudo/estudo-metodos/src/br/com/maxdoliveira/view/Tela.java
@@ -2,19 +2,63 @@ package br.com.maxdoliveira.view;
 
 import java.util.Scanner;
 
+import br.com.maxdoliveira.calc.Calc;
+
 public class Tela {
 	
 	Scanner teclado = new Scanner(System.in);
 	public Integer numUm, numDois, operador; 
 	
+	Calc C = new Calc();
+	Integer resultado;
+	
 	public void iniciar() {
 		
-		System.out.print(" Favor entrar com o Primeiro Numero!");
+		System.out.print("Entre com o Primeiro Numero!\n" + "=>>");
 		numUm = teclado.nextInt();
-		System.out.print(" Favor entrar com o Segundo Numero!");
+		System.out.print("Entre com o Segundo Numero!\n" + "=>>");
 		numDois = teclado.nextInt();
-		System.out.println("Qual operação deseja realizar?");
+		System.out.println("Qual operação deseja realizar?\n\n");
+		System.out.print(menuOperações());
 		operador = teclado.nextInt();
 		
+		
+		
+		switch (operador) {
+		case 1:
+			resultado = C.soma(numUm, numDois);
+			System.out.println("O resultado do Calculo é: " + resultado);
+			break;
+
+		case 2:
+			resultado = C.sub(numUm, numDois);
+			System.out.println("O resultado do Calculo é: " + resultado);
+			break;
+			
+		case 3:
+			resultado = C.div(numUm, numDois);
+			Integer resto = C.mod(numUm, numDois);
+			System.out.println("O resultado desta Divisão é: " + resultado + " Com resto " + resto);
+			break;
+			
+			
+		case 4:
+			resultado = C.mult(numUm, numDois);
+			System.out.println("O resultado do Calculo é: " + resultado);
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	
+	public String menuOperações() {
+		return    "1 - Soma\n"
+				+ "2 - Subtração\n"
+				+ "3 - Divisão\n"
+				+ "4 - Multiplicação\n"
+				+ "0 - Sair"
+				+ "\n=>>  ";
 	}
 }
